6671af852a71022c089ed34c31cb704111421ed3
buchanka <ninagileva14@gmail.com>
2025-09-11 22:10:45 +0300

    добавление теста cypress

---
+++ frontend/cypress.config.js
@@ -0,0 +1,11 @@
+
+
+export default {
+  e2e: {
+    baseUrl: 'http://127.0.0.1:5173',
+    specPattern: 'cypress/e2e/**/*.cy.{js,ts}',
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+};
